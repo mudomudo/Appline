9575ff7483ea157d8039b7e927576f1b29fa3764
mudomudo <irock052@gmail.com>
2021-12-25 17:04:55 +0300

    Добавил второе задание

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,14 @@
 package com.max.idea;
 
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Hello World!");
+
+
+    public static void main(String[] args){
+        double num1;
+        byte num2;
+        num1 = 1.2;
+        num2 = 127;
+        //System.out.println(num1);
+        //System.out.println(num2);
     }
 }
