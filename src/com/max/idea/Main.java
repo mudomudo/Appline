62d94801e0b997062bf0edf427670a901858dc40
mudomudo <irock052@gmail.com>
2021-12-25 18:07:36 +0300

    Добавил третье задание

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -2,13 +2,15 @@ package com.max.idea;
 
 public class Main {
 
-
     public static void main(String[] args){
-        double num1;
-        byte num2;
-        num1 = 1.2;
-        num2 = 127;
-        //System.out.println(num1);
-        //System.out.println(num2);
+    int[] arr = {1,2,3,4,5};
+
+    int tmp = arr[0];
+    arr[0] = arr[4];
+    arr[4] = tmp;
+
+    int sum = arr[0]+arr[arr.length/2];
+    System.out.println(sum);
+
     }
 }
